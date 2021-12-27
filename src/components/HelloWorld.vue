6b652e037f4c5d56bdfb50cc4856189b11c89a0d
genezhangzhendong <genezhangzhendong@didiglobal.com>
2021-12-27 17:44:20 +0800

    fix: 开始接触setup(ref onMounted watchcomputed toRefs)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    {{ msg }}
+    <div>{{ computedMsg }}</div>
+    <div>{{ name }}</div>
   </div>
 </template>
 
 <script>
+import { onMounted, ref, watch, toRefs, computed } from 'vue'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  setup(props) {
+    console.log('收到的属性值', props);
+
+    let { msg } = toRefs(props);
+    console.log('从父组件传进来的参数', msg)
+
+    let name = ref('Tom');
+    console.log('name', name)
+    console.log('name.value', name.value)
+
+    let computedMsg = computed(() => {
+      return name.value + 'computed!!!!!'
+    })
+
+    let changeName = () => {
+      setTimeout(() => {
+        name.value = 'Jack';
+      }, 2000)
+    }
+
+    watch(name, (newVal, oldValue) => {
+      console.log('name old value is', oldValue)
+      console.log('name new value is', newVal)
+    })
+
+    onMounted(changeName)
+
+    return {
+      name,
+      computedMsg
+    }
+  },
 }
 </script>
 
